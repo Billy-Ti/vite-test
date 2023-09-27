a659f604e14f8f436cbe03668b2cf6f4041a4129
Billy Ti <78161308+Billy-Ti@users.noreply.github.com>
2023-09-27 18:18:36 +0800

    Create : week0 p2

--- vite.config.js
+++ vite.config.js
@@ -24,9 +24,7 @@ function moveOutputPlugin() {
 
 
 export default defineConfig({
-  // base 的寫法：
-  // base: '/Repository 的名稱/'
-  base: '/vite-test/',
+  base: '/Front-End-Class-Batch22/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
